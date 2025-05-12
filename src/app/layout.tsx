2127e263088314e4a9c7f53692cc074cf6761b07
Kaio043 <bloxfruitsacoont@gmail.com>
2025-05-12 20:45:35 -0300

    subindo desgraca

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from "@/components/ui/toaster";
+
+export const metadata: Metadata = {
+  title: 'DevRun',
+  description: 'Um editor de código moderno com funcionalidades potencializadas por IA.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`${GeistSans.variable} ${GeistMono.variable} font-mono antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
